08cdf1d11aee2ff774324ac4c100cbd3d2614e1f
tabiosg <gutstabios@gmail.com>
2024-03-13 13:49:42 -0400

    Add simple-man's delay

--- fw/gesturalguitareffect/audiogestural.cpp
+++ fw/gesturalguitareffect/audiogestural.cpp
@@ -1,7 +1,10 @@
 #include "audiogestural.h"
 
 AudioEffectGesture::AudioEffectGesture() : AudioStream(1, inputQueueArray) {
-  mCurrentEffect = GuitarEffect::None;
+  // mCurrentEffect = GuitarEffect::None;
+  mCurrentEffect = GuitarEffect::Delay;
+  memset(mDelayBuffer, 0, DELAY_LENGTH * sizeof(int16_t));
+  mWriteIndex = 0;
 }
 
 void AudioEffectGesture::changeEffect(GuitarEffect effect) {
@@ -16,6 +19,17 @@ void AudioEffectGesture::update(void) {
     if (block != NULL) {
       // Pass the input data to the output
       transmit(block);
+      // Release the input data block
+      release(block);
+    }
+  }
+  else if (mCurrentEffect == GuitarEffect::Delay) {
+    audio_block_t *block;
+    // Check if input data is available
+    block = receiveReadOnly(0);
+    if (block != NULL) {
+      // Pass the input data to the output with delay effect
+      applyDelay(block);
       // Release the input data block
       release(block);
     }
--- fw/gesturalguitareffect/audiogestural.h
+++ fw/gesturalguitareffect/audiogestural.h
@@ -1,6 +1,7 @@
 #include <Audio.h>
 #include "guitareffects.h"
 
+#define DELAY_LENGTH 44100
 
 class AudioEffectGesture : public AudioStream {
 public:
@@ -12,6 +13,20 @@ public:
   virtual void update(void);
 
 private:
+  void applyDelay(audio_block_t *block) {
+    for (int i = 0; i < AUDIO_BLOCK_SAMPLES; i++) {
+      int16_t input = block->data[i];
+      int16_t output = mDelayBuffer[mWriteIndex];
+      mDelayBuffer[mWriteIndex] = input;
+      mWriteIndex = (mWriteIndex + 1) % DELAY_LENGTH;
+      block->data[i] = output;
+    }
+    // Output the delayed audio
+    transmit(block);
+  }
+
   GuitarEffect mCurrentEffect;
   audio_block_t *inputQueueArray[1];
+  int16_t mDelayBuffer[DELAY_LENGTH];
+  uint32_t mWriteIndex;
 };
